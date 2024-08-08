d3fc0d76bd57665061dbb6daff46d9048e482770
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-08-08 04:31:09 +0300

    login and signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg'
+
 import './App.css'
+import React, { } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginPage from './Components/LoginPage'
+import LoginForm from './Components/LoginForm'
+import Register from './Components/Register'
+
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='vh-100 gradient-custom'>
+      <div className='container'>
+        <h1 className='page-header text-center'>Login page</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/login" element={<LoginForm/>} />
+            <Route path="/register" element={<Register/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   )
 }
