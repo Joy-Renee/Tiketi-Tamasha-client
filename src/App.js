f28ea354a98d14007d07520d3a7c4fc948018bda
Neema-Charity <ncharityyy@gmail.com>
2024-08-17 13:23:57 +0300

    payments

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ function App() {
                   />
                 }
               />
-              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/payments" element={<PaymentPage />} />
             </Routes>
           </UserProvider>
         </Router>
